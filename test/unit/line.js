81afbbb7d16b977d81a7e8c8bb749f4b071a16a3
Bryan Witherspoon <witherspoon.bryan@gmail.com>
2013-06-07 15:14:04 -0400

    More line unit tests.

--- test/unit/line.js
+++ test/unit/line.js
@@ -1,11 +1,30 @@
 describe('Line', function() {
+	var line = d3.canvas.line();
+
 	it('should allow setting/getting of x/y accessors', function() {
 		var x = function(d) { return d.x; },
 		    y = function(d) { return d.y; };
 
-		var line = d3.canvas.line().x(x).y(y);
+		line.x(x).y(y);
 
 		line.x().should.equal(x);
 		line.y().should.equal(y);
+	});
+
+	it('should allow setting/getting of stroke options', function() {
+		var strokeStyle = 'steelBlue';
+
+		line
+			.strokeStyle('steelBlue')
+			.lineWidth('2px')
+			.lineCap('butt')  // hehehe butts
+			.lineJoin('miter')
+			.miterLimit(42);
+
+		line.strokeStyle().should.equal('steelBlue');
+		line.lineWidth().should.equal('2px');
+		line.lineCap().should.equal('butt');
+		line.lineJoin().should.equal('miter');
+		line.miterLimit().should.equal(42);
 	});
 });
